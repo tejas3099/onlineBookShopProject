d7ce4ee6c2df1d592d3222a1b0fbe6bb12b5129c
tejas3099 <134603655+tejas3099@users.noreply.github.com>
2023-08-29 11:05:05 +0530

    Add files via upload

---
+++ tejas_frontend/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <center><p>&copy; {new Date().getFullYear()} My Bookstore. All rights reserved.</p></center>
+    </footer>
+  );
+};
+
+export default Footer;
